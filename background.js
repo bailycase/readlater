654742013332169941bb6fc3b15d78be53291eb8
Baily Case <bailycase@Bailys-MacBook-Pro.local>
2019-03-01 00:46:15 -0600

    Corrected Listeners for Extension Scripts 😅

--- background.js
+++ background.js
@@ -15,3 +15,20 @@ chrome.runtime.onInstalled.addListener(() => {
     });
   });
 });
+chrome.runtime.onStartup.addListener(() => {
+  chrome.contextMenus.create({
+    id: 'SaveContext',
+    title: 'Save for Later',
+    contexts: ['all'],
+  });
+  chrome.contextMenus.onClicked.addListener(() => {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      const { url, title } = tabs[0];
+      chrome.storage.sync.get({ itemUrls: [] }, (result) => {
+        const urls = result.itemUrls;
+        urls.push({ url, title });
+        chrome.storage.sync.set({ itemUrls: urls, title });
+      });
+    });
+  });
+});
