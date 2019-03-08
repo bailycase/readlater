8fa7e84f0442a3e7bc06245624bb440788bd0e91
Baily Case <bailycase@Bailys-MacBook-Pro.local>
2019-03-07 20:37:38 -0600

    Removed Chrome Runtime Functions

--- background.js
+++ background.js
@@ -1,34 +1,15 @@
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.contextMenus.create({
-    id: 'SaveContext',
-    title: 'Save for Later',
-    contexts: ['all'],
-  });
-  chrome.contextMenus.onClicked.addListener(() => {
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      const { url, title } = tabs[0];
-      chrome.storage.sync.get({ itemUrls: [] }, (result) => {
-        const urls = result.itemUrls;
-        urls.push({ url, title });
-        chrome.storage.sync.set({ itemUrls: urls, title });
-      });
-    });
-  });
+chrome.contextMenus.create({
+  id: 'SaveContext',
+  title: 'Save for Later',
+  contexts: ['all'],
 });
-chrome.runtime.onStartup.addListener(() => {
-  chrome.contextMenus.create({
-    id: 'SaveContext',
-    title: 'Save for Later',
-    contexts: ['all'],
-  });
-  chrome.contextMenus.onClicked.addListener(() => {
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      const { url, title } = tabs[0];
-      chrome.storage.sync.get({ itemUrls: [] }, (result) => {
-        const urls = result.itemUrls;
-        urls.push({ url, title });
-        chrome.storage.sync.set({ itemUrls: urls, title });
-      });
+chrome.contextMenus.onClicked.addListener(() => {
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    const { url, title } = tabs[0];
+    chrome.storage.sync.get({ itemUrls: [] }, (result) => {
+      const urls = result.itemUrls;
+      urls.push({ url, title });
+      chrome.storage.sync.set({ itemUrls: urls, title });
     });
   });
 });
